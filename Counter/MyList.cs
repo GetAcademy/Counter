d9eb9c26eea7a9c5e3d84b289657726478949ad0
GET Academy <terje@getacademy.no>
2022-11-15 11:24:55 +0100

    x

---
+++ Counter/MyList.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Counter
+{
+    internal class MyList : IEnumerable<int>,
+        IEnumerator<int>
+    {
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            Current *= 2;
+            return Current < 1000;
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        public int Current { get; private set; } = 1;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- Counter/Program.cs
+++ Counter/Program.cs
@@ -1,4 +1,21 @@
 ﻿using Counter;
 
-var app = new App();
-app.Run();
+/*
+ * Ønske fra dere: GIT
+ * Enkelt eksempel på interface
+ * Polymorfisme
+ * Dependency inversion principle
+ * Ev. dependency injection
+ * Oversikt over innholdet på Moodle
+ */
+
+//var app = new App();
+//app.Run();
+
+var list = new MyList();
+foreach (var element in list)
+{
+    Console.WriteLine(element);
+}
+
+
